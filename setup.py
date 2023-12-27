b77a7473098461a32e33eecf69709e014ff7542e
cpzt <chenpan9012@gmail.com>
2023-12-27 17:41:24 +0800

    0.0.2.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='parade-manage',
-    version='0.0.2.5',
+    version='0.0.2.6',
     author='Pan Chen',
     author_email='chenpan9012@gmail.com',
     description='A manage module of parade',
